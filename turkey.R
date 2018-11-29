fdf031172ff317b40e8a3a75a9a04ed4f30e6000
smartgamer <htchd7ji@live.com>
2018-11-29 18:00:16 -0500

    com

--- turkey.R
+++ turkey.R
@@ -26,18 +26,27 @@ str(traindata[[1]][[1]][[1]])  #num [1:128] 172 34 216 110 208 46 95 66 161 125 
 
 str(traindata[[10]][[4]])
 
+traindata[[128]][[1]]
+head(trainDf[,1:5])
+tail(colnames(trainDf))
+col=paste0("ae",seq(1:16571))
+col=c(col, "isturkey", "id","end", "start")
+colnames(trainDf)=col
 
+head(colnames(trainDf))
+tail(trainDf[,16570:16575])
 
 #
 #
 # Load the party package. It will automatically load other
 # required packages.
+install.packages("party")
+install.packages("randomForest")
 library(party)
 library(randomForest)
 
 # Create the forest.
-output.forest <- randomForest(nativeSpeaker ~ age + shoeSize + score, 
-                              data = readingSkills)
+output.forest <- randomForest(isturkey ~ ., data = trainDf)
 
 # View the forest results.
 print(output.forest) 
@@ -46,44 +55,33 @@ print(output.forest)
 print(importance(fit,type = 2)) 
 
 
+#To make our formula for RF easier to manipulate
 
+var.predict<-paste(names(iris)[-5],collapse="+")
+rf.form <- as.formula(paste(names(iris)[5], var.predict, sep = " ~ "))
 
+print(rf.form)
+#This is our current itteration of the formula we're using in RF
 
+iris.rf<-randomForest(rf.form,data=iris,importance=TRUE,ntree=100)
 
+varImpPlot(iris.rf)
+#Examine our Variable importance plot
 
+to.remove<-c(which(data.frame(iris.rf$importance)$MeanDecreaseAccuracy==min(data.frame(iris.rf$importance)$MeanDecreaseAccuracy)))
+#Remove the variable with the lowest decrease in Accuracy (Least relevant variable)
 
+#Rinse, wash hands, repeat
 
+var.predict<-paste(names(iris)[-c(5,to.remove)],collapse="+")
+rf.form <- as.formula(paste(names(iris)[5], var.predict, sep = " ~ "))
 
+iris.rf<-randomForest(rf.form,data=iris,importance=TRUE,ntree=100)
 
+varImpPlot(iris.rf)
+#Examine our Variable importance plot
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+to.remove<-c(to.remove, which(data.frame(iris.rf$importance)$MeanDecreaseAccuracy==min(data.frame(iris.rf$importance)$MeanDecreaseAccuracy)))
 
 
 
